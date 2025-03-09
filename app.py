4530e2ed0422c4809dc4666a1e5030f428f727d7
shasmeen zahra <165050270+shasmeenZahra@users.noreply.github.com>
2025-03-09 18:09:44 +0500

    Update and rename c.py to app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import streamlit as st
 st.title("🤖 Computer Guesses Your Number!")
 st.write("Think of a number between 1 and 100, and I will try to guess it!")
 
-# سیشن اسٹیٹ میں ویری ایبلز محفوظ کریں تاکہ ہر ریکوئسٹ پر ڈیٹا ری سیٹ نہ ہو
 if "low" not in st.session_state:
     st.session_state.low = 1
     st.session_state.high = 100
@@ -11,10 +10,8 @@ if "low" not in st.session_state:
     st.session_state.attempts = 0
     st.session_state.message = "Is this your number?"
 
-# کمپیوٹر کا اندازہ
 st.subheader(f"Is your number {st.session_state.guess}?")
 
-# یوزر سے ریسپانس لینے کے لیے بٹن
 col1, col2, col3 = st.columns(3)
 
 with col1:
@@ -36,7 +33,6 @@ with col3:
         st.session_state.high = st.session_state.guess - 1
         st.session_state.attempts += 1
 
-# نیا گیس نکالیں
 if st.session_state.low <= st.session_state.high:
     st.session_state.guess = (st.session_state.low + st.session_state.high) // 2
 else:
